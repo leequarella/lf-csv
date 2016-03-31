43c627edcc05519797f7bc93a58d01fb095be08e
Lee Quarella <lee@lucidfrog.com>
2016-03-31 18:05:13 -0400

    added ability to tag columns as required. will kick out if required column header is not in the file. removed unused guard file added rspec tests

--- lib/lf-csv/importer.rb
+++ lib/lf-csv/importer.rb
@@ -1,33 +1,39 @@
+require 'csv'
 module LFCSV
   class Importer
-    attr_accessor :rows_count
+    attr_accessor :rows_count, :file
 
     class << self
-      def setup_headers
+      def setup_header_containers
         @header_symbols = [] unless @header_symbols
         @header_aliases = {} unless @header_aliases
       end
 
-      def column(symbol, aliases=nil)
-        setup_headers
+      def column(symbol, aliases=nil, required: false)
+        setup_header_containers
         @header_symbols << symbol unless @header_symbols.include?(symbol)
-        attach_aliases(symbol, aliases)
+        attach_aliases(symbol, aliases, required)
+        attach_required_status(symbol, required)
       end
 
-      def attach_aliases(symbol, aliases)
+      def attach_aliases(symbol, aliases, required)
         if aliases
           aliases.map! { |a| a.strip.downcase }
           add_aliases_to_column symbol, aliases
         else
-          @header_aliases[symbol] = [symbol.to_s]
+          @header_aliases[symbol] = {aliases: [symbol.to_s]}
         end
+      end
+
+      def attach_required_status(symbol, required)
+        @header_aliases[symbol][:required] = required
       end
 
       def add_aliases_to_column symbol, aliases
         if @header_aliases[symbol]
-          @header_aliases[symbol] = (@header_aliases[symbol] + aliases).uniq
+          @header_aliases[symbol] = {aliases: (@header_aliases[symbol][:aliases] + aliases).uniq}
         else
-          @header_aliases[symbol] = aliases
+          @header_aliases[symbol] = {aliases: aliases}
         end
       end
       attr_reader :header_symbols, :header_aliases
@@ -50,6 +56,7 @@ module LFCSV
         skip_blanks:  false)
       @rows_count = rows.count
       header_index = parse_headers(rows.headers)
+      return if missing_required_headers?(rows.headers)
       rows.each {|row| process_csv_row(row, header_index)}
     end
 
@@ -77,13 +84,31 @@ module LFCSV
       search_string = header_string.strip.downcase
       matched = false
       self.class.header_symbols.each do |header_symbol|
-        if self.class.header_aliases[header_symbol].include?(search_string)
+        if self.class.header_aliases[header_symbol][:aliases].include?(search_string)
           matched = true
           @header_index[header_symbol] = index
           break
         end
       end
       matched
+    end
+
+    def missing_required_headers?(file_headers)
+      missing_headers = required_headers - file_headers
+      if missing_headers.any?
+        missing_column_headers(missing_headers)
+        true
+      else
+        false
+      end
+    end
+
+    def required_headers
+      required = []
+      self.class.header_aliases.each do |symbol, info|
+        required << symbol if info[:required] == true
+      end
+      required
     end
 
     def process_csv_row(row, header_index)
@@ -111,6 +136,10 @@ module LFCSV
     def handle_row(row)
       raise "No method has been defined for handling this file.
              You must define a method in the class inheriting from LSCSV::Importer."
+    end
+
+    def missing_column_header
+      raise "Required columns are missing from this file."
     end
 
   end
---
+++ spec/importer_spec.rb
@@ -0,0 +1,64 @@
+require 'lf-csv/importer'
+
+describe LFCSV::Importer, "#Importer" do
+  let(:csv_file){ File.read("spec/support/test.csv") }
+  describe "column definition:" do
+    it "allows columns to be defined" do
+      LFCSV::Importer.column :column_a
+      expect(LFCSV::Importer.header_aliases).to eq({
+        column_a: {
+          aliases: ["column_a"],
+          required: false
+        }
+      })
+    end
+
+    it "allows columns to be defined with aliases" do
+      LFCSV::Importer.column :column_a, ["column a"]
+      expect(LFCSV::Importer.header_aliases).to eq({
+        column_a:{
+          aliases: ["column_a", "column a"],
+          required: false
+        }
+      })
+    end
+
+    it "allows columns to be tagged as `required`" do
+      LFCSV::Importer.column :column_a, required: true
+      expect(LFCSV::Importer.header_aliases).to eq({
+        column_a: {
+          aliases: ["column_a"],
+          required: true
+        }
+      })
+    end
+  end
+
+  it "can be initialized with data" do
+    importer = LFCSV::Importer.new(file: "A")
+    expect(importer.file).to eq "A"
+  end
+
+  describe "processing csv files: " do
+    it "converts rows from a csv into hashes and passes them to `handle_row`" do
+      LFCSV::Importer.column :column_a, ["column a"]
+      importer = LFCSV::Importer.new(file: csv_file)
+
+      expect(importer).to receive(:handle_row).with({
+        column_a: "data from a"
+      })
+      importer.process
+    end
+
+    context "when a required header is missing" do
+      it "calls `missing_column_header` and does not process any rows" do
+        LFCSV::Importer.column :a_missing_column, required: true
+        importer = LFCSV::Importer.new(file: csv_file)
+
+        expect(importer).to_not receive(:handle_row)
+        expect(importer).to receive(:missing_column_headers).with([:a_missing_column])
+        importer.process
+      end
+    end
+  end
+end
